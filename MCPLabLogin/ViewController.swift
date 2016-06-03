c01c33bc1b3f168ae9bb86b1d28445411c58a915
riad <khr.1380@gmail.com>
2016-06-03 10:08:16 +0200

    Initial Commit

---
+++ MCPLabLogin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MCPLabLogin
+//
+//  Created by Khan hafizur rahman on 6/3/16.
+//  Copyright © 2016 Khan hafizur rahman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
